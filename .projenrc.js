00f711d54a524b8f80cab65a3cc28b0707546741
Neil Guan <guan840912@gmail.com>
2021-06-28 14:29:33 +0800

    bump projen version

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,8 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
 const PROJECT_NAME = 'cdk-ecr-image-scan-notify';
 const PROJECT_DESCRIPTION = 'cdk-ecr-image-scan-notify is an AWS CDK construct library that notify the slack channel of Amazon ECR image scan results';
-const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
-const CDK_VERSION = '1.88.0';
+const CDK_VERSION = '1.110.0';
 const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
@@ -11,13 +10,21 @@ const project = new AwsCdkConstructLibrary({
   authorAddress: 'hayaok333@gmail.com',
   authorName: 'hayao-k',
   keywords: ['aws', 'cdk', 'ecr-image', 'scan-notify'],
-  dependabot: false,
   compat: true,
   defaultReleaseBranch: 'main',
-  releaseBranches: ['main'],
-  projenUpgradeSecret: AUTOMATION_TOKEN,
   stability: 'experimental',
   cdkVersion: CDK_VERSION,
+  autoDetectBin: false,
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve'],
+      secret: 'AUTOMATION_GITHUB_TOKEN',
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['neilkuan', 'hayao-k'],
+  },
   cdkDependencies: [
     '@aws-cdk/aws-iam',
     '@aws-cdk/aws-lambda',
@@ -31,44 +38,7 @@ const project = new AwsCdkConstructLibrary({
     module: 'cdk_ecr_image_scan_notify',
   },
 });
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage'];
 project.gitignore.exclude(...common_exclude);
